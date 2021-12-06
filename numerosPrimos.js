40f6f65ccb95c337c03d264d40e5d9dcb5cd4ec2
Viviane Brenner <88917989+VivianeBrenner@users.noreply.github.com>
2021-12-06 16:37:31 -0300

    Update numerosPrimos.js

--- numerosPrimos.js
+++ numerosPrimos.js
@@ -5,19 +5,19 @@ let primosEncontrados = 0
 let dividendo
 let ehPrimo
 
-for(dividendo = 2; primosEncontrados < 1001; dividendo++){      // div = 4 / 2 < 1001
-    ehPrimo = true                                              //ehp = t
-    for(let divisor = 2; dividendo > divisor; divisor++){            //divi = 2 / 4 > 2 /
-        if(dividendo % divisor == 0) {                              //ehP = f
-            ehPrimo = false;                                        //4 dividido por 2 == 0                         
+for(dividendo = 2; primosEncontrados < 1001; dividendo++){      
+    ehPrimo = true                                             
+    for(let divisor = 2; dividendo > divisor; divisor++){            
+        if(dividendo % divisor == 0) {                              
+            ehPrimo = false;                                                      
             break
 
         }                            
       
     }
 
-    if(ehPrimo == true){                                            // mentira
-        primosEncontrados++                                         // pE = 2
+    if(ehPrimo == true){                                            
+        primosEncontrados++                                       
     }
 }
 
